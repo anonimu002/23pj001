0c93b8ce7ea2883f0df820c5791491ac50bf5a1f
anonimu002 <148605288+anonimu002@users.noreply.github.com>
2023-10-26 00:37:25 +0900

    Add files via upload

--- Timer.h
+++ Timer.h
@@ -7,12 +7,13 @@
 elapsedMillis Timer_TempHumid;          //온습도 측정 타이머
 elapsedMillis Timer_Air;                //공기 질 측정 타이머
 elapsedMillis Timer_ButtonPress;        //버튼 눌린 타이머
+elapsedMillis Timer_LCDchange;          //LCD 갱신 타이머
 elapsedMillis Timer_Human;              //인체감지 타이머 1(인체감지 측정 타이머)
 elapsedMillis Timer_HumanDetect;        //인체감지 타이머 2(미감지 스톱워치)
 elapsedMillis Timer_Htime;              //인체감지 스톱워치 타이머
 
 #define TempHumidInterval 5000            //온습도 측정 주기(5초)
 #define AirInterval 1000                  //공기 질 측정 주기(1초)
-#define LCDinterval 5000                  //LCD 전환 주기(5초)
+#define LCDinterval 1000                  //LCD 갱신 주기(1초)
 #define HumanInterval 100                //인체감지 측정 주기(1초)
 #define HtimeInterval 1000                 //인체감지 스톱워치 계산 주기(1초)
